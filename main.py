3231b4642f6dbe84099967fd16fb2d5455cce651
hooozo <mozafarth@gmail.com>
2025-05-15 23:01:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Welcome to the YouTube Channel Name Generator:')
+user_name = input('What is your nickname?\n')
+content = input('What is your channel about?\n')
+print('You could name your channel('+content+'with'+user_name)
